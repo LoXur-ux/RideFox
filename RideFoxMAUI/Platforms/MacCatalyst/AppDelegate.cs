582e6c9b66cff44a973b1ab84e6df511c37bea83
LoXur-ux <starkaw@ya.ru>
2023-09-09 13:39:56 +0300

    Добавьте файлы проекта.

---
+++ RideFoxMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RideFoxMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
